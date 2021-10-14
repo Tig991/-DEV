412aa862ce53745a2f696bbda84b762e239b0503
Tiago Santos da Silva <92393774+Tig991@users.noreply.github.com>
2021-10-14 18:27:48 -0300

    Add files via upload

---
+++ ParImpar.java
@@ -0,0 +1,18 @@
+package cest.edu.br.ParImpar;
+
+import javax.swing.JOptionPane;
+
+public class ParImpar {
+
+public static void main(String[] args) {
+		int number = Integer.parseInt(javax.swing.JOptionPane.showInputDialog(null, "Enter one number"));
+		
+		if(number % 2 ==0) {
+			System.out.println("O número % é par!"); 
+			
+		} else {
+				System.out.println("O número % é ímpar");
+				
+		}		
+	}
+}
